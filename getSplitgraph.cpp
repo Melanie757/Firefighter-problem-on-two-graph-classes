5e427636207dcb550ecca9c9f16e19325c93db3a
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2017-07-04 10:20:03 +0200

    fixed random number generator

--- getSplitgraph.cpp
+++ getSplitgraph.cpp
@@ -3,19 +3,17 @@
 #include <random>
 #include <algorithm>
 
-int RandU (int min, int max) {
-	std::random_device rseed;
-	std::mt19937 rgen(rseed());
-	std::uniform_int_distribution<int> idist(min,max);
-	return idist(rgen);
-}
-
 //input: c   = number of vertices in the clique,
 //		 ind = number of vertices in the independent set
 Splitgraph getSplitgraph(int c, int ind) {
 	Graph g;
 	g.grow(c + ind);
-	for (int i=0; i<c-1; ++i) {
+	std::random_device rseed;
+	std::mt19937 rgen(rseed());
+	std::uniform_int_distribution<> idist(1,c-1);
+	std::uniform_int_distribution<> idist2(0,c);
+  
+  for (int i=0; i<c-1; ++i) {
 		Vertex & v = g.at(i);
 		for (int j=i+1; j<c; ++j) {
 			v.addlink(j);
@@ -25,11 +23,11 @@ Splitgraph getSplitgraph(int c, int ind) {
 	}
 
 	for (int i=c; i<g.size(); ++i) {
-		Vertex & v = g.at(i);
-		int r = RandU(1,c-1);
+    Vertex & v = g.at(i);
+		int r = idist(rgen);
 		int j = 0;
 		while(j<r){
-			int dest = RandU(0,c);
+			int dest = idist2(rgen);
 			const std::vector<int> adj = v.getAdjlist();
 			if(std::find(adj.begin(), adj.end(), dest) != adj.end()) {
 				continue;
